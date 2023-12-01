741fd74b288c1b62018b575f5301bcf989a51e57
Shankar <120117175+Saanusalvi1@users.noreply.github.com>
2023-12-01 18:23:56 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,7 +1,7 @@
 api_id = 20088962
 api_hash = "257f47d347157555890a64b12bc0134f"
-bot_token = "6778587387:AAFt5GDWxQjXXM6JX6MB1TTNKYPDivrvK4k"
+bot_token = "6743557600:AAF3XJS-eMy426laFRt8YCUJ0AupKnbdgk4"
 auth_users = [6169016546]
-sudo_user = -1004008382140
+sudo_user = -1002059297194
 log_channel = -1002082838226
 txt_channel = -1002082838226
