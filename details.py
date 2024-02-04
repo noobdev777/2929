0fc461eee81800295577cc60133b9ddd40c933d4
Shankar <120117175+Saanusalvi1@users.noreply.github.com>
2024-02-04 23:17:33 +0530

    Update details.py

--- details.py
+++ details.py
@@ -5,5 +5,5 @@ api_hash = "257f47d347157555890a64b12bc0134f"
 bot_token = "6743557600:AAF3XJS-eMy426laFRt8YCUJ0AupKnbdgk4"
 auth_users = [6169016546]
 sudo_user = -1002059297194
-log_channel = -1002082838226
-txt_channel = -1002082838226
+log_channel = -1001851582041
+txt_channel = -1001851582041
