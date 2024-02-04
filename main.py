86174a5eebb2dca72d9068687f185c64bb309c1c
Shankar <120117175+Saanusalvi1@users.noreply.github.com>
2024-02-04 23:34:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -46,8 +46,8 @@ bot = Client(
     api_hash= "257f47d347157555890a64b12bc0134f",
     auth_users = "6169016546", 
     sudo_user = "-1002059297194", 
-    log_channel = "-1002082838226", 
-    txt_channel = "-1002082838226", 
+    log_channel = "-1001851582041", 
+    txt_channel = "-1001851582041", 
     bot_token= "6778587387:AAFt5GDWxQjXXM6JX6MB1TTNKYPDivrvK4k")
       
 @bot.on_message(filters.command(["start"])&(filters.chat(auth_users)))
