e50190804d6c57fe0ae1cdea433f2b00087f880b
noobdev777 <164876911+noobdev777@users.noreply.github.com>
2024-03-30 14:29:17 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,9 +1,9 @@
 import os
 
-api_id = 20088962
-api_hash = "257f47d347157555890a64b12bc0134f"
-bot_token = "6743557600:AAGdbZ-rgsgGBm85QXofKeUYBfk5fpRAv8I"
-auth_users = [6169016546]
-sudo_user = -1002059297194
-log_channel = -1001851582041
-txt_channel = -1001851582041
+api_id = 25046127
+api_hash = "b98e578e8b500c77c6a80839dfbdc4a5"
+bot_token = "7107104145:AAGpdOR5GWT0wxwCttrXvqfwmSZ0BO4mZqE"
+auth_users = [6222170405]
+sudo_user = -1002102444164
+log_channel = -1002017654652
+txt_channel = -1002017654652
